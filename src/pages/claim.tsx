2c895f8ddab6e6644526f8e6a9dbeb96b56bc44c
Rashmi-278 <rashmiabbigeri@gmail.com>
2024-07-05 14:05:08 +0200

    build err fix

--- src/pages/claim.tsx
+++ src/pages/claim.tsx
@@ -62,7 +62,8 @@ export default function Claim() {
                         {session ? (
                             <Button colorScheme="teal" variant={"ghost"} size="md" px={7} py={2} mt={2} onClick={() => signOut()}>
                                 Hello, {session.user.name}.
-                                Your user ID is {session.user.id}
+                                {/* @ts-ignore */}
+                                Your user ID is {session.user?.id!}
                             </Button>
                         ) : (
                             <Button colorScheme="teal" variant={"outline"} size="md" px={4} py={2} mt={2} onClick={() => signIn('twitter')}>
